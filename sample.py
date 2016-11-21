b3184874bbe28347af680e6751f2d791f28da555
manikanta.allaki <manikanta.allaki@gramener.com>
2016-11-21 18:32:54 +0530

    Sample bar chart with sample data

---
+++ sample.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask import render_template
+import pandas as pd
+import json
+app = Flask(__name__)
+
+@app.route('/hello/')
+def hello(name=None):
+	df = pd.read_csv('bar-data.csv')
+	data = df.to_json(orient='records')
+	return render_template('index.html',data=data)
+
+
+ 
+if __name__ == '__main__':
+    app.run(threaded=True,
+    host='localhost'
+)
