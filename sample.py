b7f881879588373bb6cf161948ab31c7fc3cd0bd
manikanta.allaki <manikanta.allaki@gramener.com>
2016-11-21 19:22:50 +0530

    Changed url mapping

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ import pandas as pd
 import json
 app = Flask(__name__)
 
-@app.route('/hello/')
+@app.route('/')
 def hello(name=None):
 	df = pd.read_csv('bar-data.csv')
 	data = df.to_json(orient='records')
